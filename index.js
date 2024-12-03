2a876cc0a1c0e8ef6ee2d1f4d20b9ab70126a16a
JENNIFER974-DEL <jennifernyaguthii23@gmail.com>
2024-12-03 07:57:29 +0300

    done lab

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
-function distanceFromHqInBlocks(someValue) {
-    return Math.abs(someValue - 42);
+function distanceFromHqInBlocks(pickupLocation) {
+  const hqLocation = 42; 
+  return Math.abs(pickupLocation - hqLocation); 
+}
+function distanceFromHqInFeet(pickupLocation) {
+  const blocks = distanceFromHqInBlocks(pickupLocation);
+  const feetPerBlock = 264;
+  return blocks * feetPerBlock;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+  const feetPerBlock = 264;
+  const blocksTravelled = Math.abs(endBlock - startBlock); 
+  return blocksTravelled * feetPerBlock;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = Math.abs(destination - start);
+  const feetPerBlock = 264;
+  let farePrice;
+}
+function calculatesFarePrice(start, destination) {
+  const distance = Math.abs(destination - start);
+  const feetPerBlock = 264;
+  let farePrice;
+
+}
+function calculatesFarePrice(start,destination){
+  let distance = distanceTravelledInFeet(start,destination);
+  if (distance<= 400){
+    return 0;
   }
-  
-  function distanceFromHqInFeet(someValue) {
-    return distanceFromHqInBlocks(someValue) * 264;
+  if (distance>=400 && distance<=2000){
+    return (distance-400)* 0.02;
   }
-  
-  function distanceTravelledInFeet(start, destination) {
-    return Math.abs(destination - start) * 264;
+  else if(distance>=2000&& distance<=2500){
+    return 25;
   }
-  
-  function calculatesFarePrice(start, destination) {
-    const distance = distanceTravelledInFeet(start, destination);
-  
-    if (distance <= 400) {
-      return 0;
-    } else if (distance > 400 && distance <= 2000) {
-      return (distance - 400) * 0.02;  
-    } else if (distance > 2000 && distance <= 2500) {
-      return 25;  
-    } else {
-      return 'cannot travel that far'; 
-    }
+  else if (distance > 2500){
+    return "cannot travel that far";
   }
+}
+
   
